167d39e37eb44877efd259f0e32b6d7a5299e663
Chen Xingyu <hi@xingrz.me>
2023-10-24 12:40:32 +0800

    drivers: pinctrl: Implement pin controller for WCH CH5xx series
    This commit introduces a pinctrl driver for the CH5xx series.
    
    The limitations of pin multiplexing of WCH CH5xx series are described
    in the binding configuration.
    
    Signed-off-by: Chen Xingyu <hi@xingrz.me>

---
+++ soc/wch/ch32v/qingke_v3/pinctrl_soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023-2024 Chen Xingyu <hi@xingrz.me>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_RISCV_CH32V_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_RISCV_CH32V_PINCTRL_SOC_H_
+
+#if defined(CONFIG_SOC_CH565) || defined(CONFIG_SOC_CH569)
+#include <pinctrl_soc_ch5xx.h>
+#endif /* CONFIG_SOC_CH565 || CONFIG_SOC_CH569 */
+
+#endif /* ZEPHYR_SOC_RISCV_CH32V_PINCTRL_SOC_H_ */
