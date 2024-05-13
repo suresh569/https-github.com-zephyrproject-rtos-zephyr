zephyrproject-rtos/zephyr

drivers: adc: Place API into iterable section
Move all adc driver api structs into an iterable section, this allows us
to verify if an api pointer is located in compatible linker section.

Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

drivers/adc/adc_ads1x1x.c
@@ -585,7 +585,7 @@ static int ads1x1x_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api ads1x1x_api = {
+static DRIVER_API(adc_driver_api, ads1x1x_api) = {
 	.channel_setup = ads1x1x_channel_setup,
 	.read = ads1x1x_read,
 	.ref_internal = 2048,

drivers/adc/adc_ads7052.c
@@ -284,7 +284,7 @@ static int adc_ads7052_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api ads7052_api = {
+static DRIVER_API(adc_driver_api, ads7052_api) = {
 	.channel_setup = adc_ads7052_channel_setup,
 	.read = adc_ads7052_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_gd32.c
@@ -356,7 +356,7 @@ static int adc_gd32_read_async(const struct device *dev,
 }
 #endif /* CONFIG_ADC_ASYNC */
 
-static struct adc_driver_api adc_gd32_driver_api = {
+static DRIVER_API(adc_driver_api, adc_gd32_driver_api) = {
 	.channel_setup = adc_gd32_channel_setup,
 	.read = adc_gd32_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_numaker.c
@@ -291,7 +291,7 @@ static int adc_numaker_read_async(const struct device *dev,
 }
 #endif
 
-static const struct adc_driver_api adc_numaker_driver_api = {
+static DRIVER_API(adc_driver_api, adc_numaker_driver_api) = {
 	.channel_setup = adc_numaker_channel_setup,
 	.read = adc_numaker_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_sam_afec.c
@@ -331,7 +331,7 @@ static int adc_sam_read_async(const struct device *dev,
 }
 #endif
 
-static const struct adc_driver_api adc_sam_api = {
+static DRIVER_API(adc_driver_api, adc_sam_api) = {
 	.channel_setup = adc_sam_channel_setup,
 	.read = adc_sam_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_smartbond_gpadc.c
@@ -282,7 +282,7 @@ static int adc_smartbond_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api adc_smartbond_driver_api = {
+static DRIVER_API(adc_driver_api, adc_smartbond_driver_api) = {
 	.channel_setup = adc_smartbond_channel_setup,
 	.read          = adc_smartbond_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_vf610.c
@@ -233,7 +233,7 @@ static int vf610_adc_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api vf610_adc_driver_api = {
+static DRIVER_API(adc_driver_api, vf610_adc_driver_api) = {
 	.channel_setup = vf610_adc_channel_setup,
 	.read = vf610_adc_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_xmc4xxx.c
@@ -300,7 +300,7 @@ static int adc_xmc4xxx_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api api_xmc4xxx_driver_api = {
+static DRIVER_API(adc_driver_api, api_xmc4xxx_driver_api) = {
 	.channel_setup = adc_xmc4xxx_channel_setup,
 	.read = adc_xmc4xxx_read,
 #ifdef CONFIG_ADC_ASYNC

suresh569/https-github.com-zephyrproject-rtos-zephyr

drivers/adc/adc_ads1x1x.c
@@ -585,7 +585,7 @@ static int ads1x1x_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api ads1x1x_api = {
+static DEVICE_API(adc, ads1x1x_api) = {
 	.channel_setup = ads1x1x_channel_setup,
 	.read = ads1x1x_read,
 	.ref_internal = 2048,

drivers/adc/adc_ads7052.c
@@ -284,7 +284,7 @@ static int adc_ads7052_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api ads7052_api = {
+static DEVICE_API(adc, ads7052_api) = {
 	.channel_setup = adc_ads7052_channel_setup,
 	.read = adc_ads7052_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_gd32.c
@@ -356,7 +356,7 @@ static int adc_gd32_read_async(const struct device *dev,
 }
 #endif /* CONFIG_ADC_ASYNC */
 
-static struct adc_driver_api adc_gd32_driver_api = {
+static DEVICE_API(adc, adc_gd32_driver_api) = {
 	.channel_setup = adc_gd32_channel_setup,
 	.read = adc_gd32_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_numaker.c
@@ -301,7 +301,7 @@ static int adc_numaker_read_async(const struct device *dev,
 }
 #endif
 
-static const struct adc_driver_api adc_numaker_driver_api = {
+static DEVICE_API(adc, adc_numaker_driver_api) = {
 	.channel_setup = adc_numaker_channel_setup,
 	.read = adc_numaker_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_sam_afec.c
@@ -331,7 +331,7 @@ static int adc_sam_read_async(const struct device *dev,
 }
 #endif
 
-static const struct adc_driver_api adc_sam_api = {
+static DEVICE_API(adc, adc_sam_api) = {
 	.channel_setup = adc_sam_channel_setup,
 	.read = adc_sam_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_smartbond_gpadc.c
@@ -282,7 +282,7 @@ static int adc_smartbond_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api adc_smartbond_driver_api = {
+static DEVICE_API(adc, adc_smartbond_driver_api) = {
 	.channel_setup = adc_smartbond_channel_setup,
 	.read          = adc_smartbond_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_vf610.c
@@ -233,7 +233,7 @@ static int vf610_adc_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api vf610_adc_driver_api = {
+static DEVICE_API(adc, vf610_adc_driver_api) = {
 	.channel_setup = vf610_adc_channel_setup,
 	.read = vf610_adc_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_xmc4xxx.c
@@ -300,7 +300,7 @@ static int adc_xmc4xxx_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api api_xmc4xxx_driver_api = {
+static DEVICE_API(adc, api_xmc4xxx_driver_api) = {
 	.channel_setup = adc_xmc4xxx_channel_setup,
 	.read = adc_xmc4xxx_read,
 #ifdef CONFIG_ADC_ASYNC