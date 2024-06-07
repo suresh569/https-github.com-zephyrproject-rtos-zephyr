ShatQQ/zephyr

sca: Add ECLAIR SCA configuration files.
Add the Eclair configuration files, which are needed to
configure the static code analysis tool.

Signed-off-by: Simon Hein <Shein@baumer.com>

cmake/sca/eclair/ECL/analysis.ecl
@@ -1,0 +1,14 @@
+-project_name=getenv("ECLAIR_PROJECT_NAME")
+-project_root=getenv("ECLAIR_PROJECT_ROOT")
+-setq=data_dir,getenv("ECLAIR_DATA_DIR")
+-setq=set,getenv("ECLAIR_RULES_SET")
+
+-enable=B.REPORT.ECB
+-config=B.REPORT.ECB,output=join_paths(data_dir,"FRAME.@FRAME@.ecb")
+-config=B.REPORT.ECB,preprocessed=show
+-config=B.REPORT.ECB,macros=10
+
+-enable=B.EXPLAIN
+
+-eval_file=concat("analysis_", set, ".ecl")
+-eval_file=common_config.ecl

suresh569/https-github.com-zephyrproject-rtos-zephyr

cmake/sca/eclair/ECL/analysis.ecl
@@ -1,0 +1,14 @@
+-project_name=getenv("ECLAIR_PROJECT_NAME")
+-project_root=getenv("ECLAIR_PROJECT_ROOT")
+-setq=data_dir,getenv("ECLAIR_DATA_DIR")
+-setq=set,getenv("ECLAIR_RULESET")
+
+-enable=B.REPORT.ECB
+-config=B.REPORT.ECB,output=join_paths(data_dir,"FRAME.@FRAME@.ecb")
+-config=B.REPORT.ECB,preprocessed=show
+-config=B.REPORT.ECB,macros=10
+
+-enable=B.EXPLAIN
+
+-eval_file=concat("analysis_", set, ".ecl")
+-eval_file=common_config.ecl