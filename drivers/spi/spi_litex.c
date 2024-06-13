kz-space/zephyr

drivers: spi: litex: rework spi driver
rework the litex spi driver.

Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>

drivers/spi/spi_litex.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019 Antmicro <www.antmicro.com>
+ * Copyright (c) 2024 Vogl Electronic GmbH
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,21 +9,55 @@
 
 #define LOG_LEVEL CONFIG_SPI_LOG_LEVEL
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(spi_litespi);
-#include "spi_litespi.h"
-#include <stdbool.h>
+LOG_MODULE_REGISTER(spi_litex);
+
+#include "spi_litex_common.h"
+
+#define REG_CONTROL_LENGTH_OFFSET 1
+#define REG_CS_MODE_OFFSET 1
+
+struct spi_litex_data {
+	struct spi_context ctx;
+	uint8_t dfs;	/* dfs in bytes: 1,2 or 4 */
+};
+
+struct spi_litex_cfg {
+	uint32_t control_addr;
+	uint32_t status_addr;
+	uint32_t mosi_addr;
+	uint32_t miso_addr;
+	uint32_t cs_addr;
+	uint32_t loopback_addr;
+	uint32_t clk_divider_addr;
+	bool clk_divider_exists;
+	int data_width;
+	int max_cs;
+};
+
+static void spi_set_frequency(const struct device *dev, const struct spi_config *config)
+{
+	const struct spi_litex_cfg *dev_config = dev->config;
+
+	if (!dev_config->clk_divider_exists) {
+		/* The clk_divider is optional, thats why we check. */
+		LOG_WRN("No clk_divider found, can't change frequency");
+		return;
+	}
+
+	uint16_t divisor = DIV_ROUND_UP(LITEX_CPU_FREQ, config->frequency);
+
+	litex_write16(divisor, dev_config->clk_divider_addr);
+}
 
 /* Helper Functions */
-static int spi_config(const struct spi_config *config, uint16_t *control)
+static int spi_config(const struct device *dev, const struct spi_config *config)
 {
-	uint8_t cs = 0x00;
+	const struct spi_litex_cfg *dev_config = dev->config;
+	struct spi_litex_data *dev_data = dev->data;
 
-	if (config->slave != 0) {
-		if (config->slave >= SPI_MAX_CS_SIZE) {
-			LOG_ERR("More slaves than supported");
-			return -ENOTSUP;
-		}
-		cs = (uint8_t)(config->slave);
+	if (config->slave >= dev_config->max_cs) {
+		LOG_ERR("More slaves than supported");
+		return -ENOTSUP;
 	}
 
 	if (config->operation & SPI_HALF_DUPLEX) {
@@ -30,8 +65,8 @@ static int spi_config(const struct spi_config *config, uint16_t *control)
 		return -ENOTSUP;
 	}
 
-	if (SPI_WORD_SIZE_GET(config->operation) != 8) {
-		LOG_ERR("Word size must be %d", SPI_WORD_SIZE);
+	if (SPI_WORD_SIZE_GET(config->operation) <= dev_config->data_width) {
+		LOG_ERR("Word size must be <= %d", dev_config->data_width);
 		return -ENOTSUP;
 	}
 
@@ -67,122 +102,148 @@ static int spi_config(const struct spi_config *config, uint16_t *control)
 	}
 
 	/* Set Loopback */
-	if (config->operation & SPI_MODE_LOOP) {
-		litex_write8(SPI_ENABLE, SPI_LOOPBACK_ADDR);
+	if (!litex_read8(dev_config->loopback_addr) != !(config->operation & SPI_MODE_LOOP)) {
+		litex_write8((config->operation & SPI_MODE_LOOP) ? 0x1 : 0x0,
+			     dev_config->loopback_addr);
 	}
+
+	/* Set CS mode, this way it is handeled by this driver. */
+	litex_write8(0x1, dev_config->cs_addr + REG_CS_MODE_OFFSET);
+
 	/* Set word size */
-	*control = (uint16_t) (SPI_WORD_SIZE_GET(config->operation)
-			<< POSITION_WORD_SIZE);
-	/* Write configurations */
-	litex_write8(cs, SPI_CS_ADDR);
-	litex_write16(*control, SPI_CONTROL_ADDR);
+	litex_write8(SPI_WORD_SIZE_GET(config->operation),
+		     dev_config->control_addr + REG_CONTROL_LENGTH_OFFSET);
+
+	dev_data->dfs = get_dfs_value(dev_data->ctx);
+
+	spi_set_frequency(dev, config);
 
 	return 0;
 }
 
-static void spi_litespi_send(const struct device *dev, uint8_t frame,
-		             uint16_t control)
+static void spi_litex_send(const struct device *dev, uint8_t len, uint32_t frame)
 {
+	const struct spi_litex_cfg *dev_config = dev->config;
 	/* Write frame to register */
-	litex_write8(frame, SPI_MOSI_DATA_ADDR);
+	litex_write(dev_config->mosi_addr, len, frame);
 	/* Start the transfer */
-	litex_write16(control | SPI_ENABLE, SPI_CONTROL_ADDR);
+	litex_write8(0x1, dev_config->control_addr);
 	/* Wait until the transfer ends */
-	while (!(litex_read8(SPI_STATUS_ADDR)))
-		;
-}
-
-static uint8_t spi_litespi_recv(void)
-{
-    /* Return data inside MISO register */
-	return litex_read8(SPI_MISO_DATA_ADDR);
-}
-
-static void spi_litespi_xfer(const struct device *dev,
-			     const struct spi_config *config,
-			     uint16_t control)
-{
-	struct spi_context *ctx = &SPI_DATA(dev)->ctx;
-	uint32_t send_len = spi_context_longest_current_buf(ctx);
-	uint8_t read_data;
-
-	for (uint32_t i = 0; i < send_len; i++) {
-		/* Send a frame */
-		if (i < ctx->tx_len) {
-			spi_litespi_send(dev, (uint8_t) (ctx->tx_buf)[i],
-					control);
-		} else {
-			/* Send dummy bytes */
-			spi_litespi_send(dev, 0, control);
-		}
-		/* Receive a frame */
-		read_data = spi_litespi_recv();
-		if (i < ctx->rx_len) {
-			ctx->rx_buf[i] = read_data;
-		}
+	while (!(litex_read8(dev_config->status_addr))) {
+		;/* Wait */
 	}
-	spi_context_complete(ctx, dev, 0);
+}
+
+static uint32_t spi_litex_recv(const struct device *dev, uint8_t len)
+{
+	const struct spi_litex_cfg *dev_config = dev->config;
+	struct spi_litex_data *dev_data = dev->data;
+
+	/* Return data inside MISO register */
+	return litex_read(dev_config->miso_addr, len);
+}
+
+static void spi_litex_xfer(const struct device *dev, const struct spi_config *config)
+{
+	const struct spi_litex_cfg *dev_config = dev->config;
+	struct spi_litex_data *dev_data = dev->data;
+	struct spi_context *ctx = dev_data->ctx;
+	uint32_t txd, rxd;
+
+	/* Set CS */
+	litex_write8(BIT(config->cs), dev_config->cs_addr);
+
+	do {
+		/* Send a frame */
+		if (spi_context_tx_buf_on(ctx)) {
+			txd = *ctx->tx_buf;
+		} else {
+			txd = 0U;
+		}
+
+		spi_litex_send(dev, txd, dev_data->dfs);
+
+		spi_context_update_tx(ctx, dev_data->dfs, 1);
+
+		rxd = spi_litex_recv(dev, dev_data->dfs);
+
+		if (spi_context_rx_buf_on(ctx)) {
+			*ctx->rx_buf = rxd;
+		}
+
+		spi_context_update_rx(ctx, dev_data->dfs, 1);
+
+	} while (spi_context_tx_on(ctx) || spi_context_rx_on(ctx));
+
+	/* Clear CS */
+	litex_write8(0x0, dev_config->cs_addr);
 }
 
 /* API Functions */
 
-static int spi_litespi_transceive(const struct device *dev,
-				  const struct spi_config *config,
-				  const struct spi_buf_set *tx_bufs,
-				  const struct spi_buf_set *rx_bufs)
+static int spi_litex_transceive(const struct device *dev, const struct spi_config *config,
+				const struct spi_buf_set *tx_bufs,
+				const struct spi_buf_set *rx_bufs)
 {
-	uint16_t control = 0;
+	struct spi_litex_data *dev_data = dev->data;
+	int ret = 0;
 
-	spi_config(config, &control);
-	spi_context_buffers_setup(&SPI_DATA(dev)->ctx, tx_bufs, rx_bufs, 1);
-	spi_litespi_xfer(dev, config, control);
+	ret = spi_config(dev, config);
+	if (ret < 0) {
+		return ret;
+	}
+	spi_context_buffers_setup(dev_data->ctx, tx_bufs, rx_bufs, dev_data->dfs);
+	spi_litex_xfer(dev, config);
 	return 0;
 }
 
 #ifdef CONFIG_SPI_ASYNC
-static int spi_litespi_transceive_async(const struct device *dev,
-					const struct spi_config *config,
-					const struct spi_buf_set *tx_bufs,
-					const struct spi_buf_set *rx_bufs,
-					struct k_poll_signal *async)
+static int spi_litex_transceive_async(const struct device *dev, const struct spi_config *config,
+				      const struct spi_buf_set *tx_bufs,
+				      const struct spi_buf_set *rx_bufs,
+				      struct k_poll_signal *async)
 {
 	return -ENOTSUP;
 }
 #endif /* CONFIG_SPI_ASYNC */
 
-static int spi_litespi_release(const struct device *dev,
-			       const struct spi_config *config)
+static int spi_litex_release(const struct device *dev, const struct spi_config *config)
 {
-	if (!(litex_read8(SPI_STATUS_ADDR))) {
+	const struct spi_litex_cfg *dev_config = dev->config;
+
+	if (!(litex_read8(dev_config->status_addr))) {
 		return -EBUSY;
 	}
 	return 0;
 }
 
 /* Device Instantiation */
-static const struct spi_driver_api spi_litespi_api = {
-	.transceive = spi_litespi_transceive,
+static const struct spi_driver_api spi_litex_api = {
+	.transceive = spi_litex_transceive,
 #ifdef CONFIG_SPI_ASYNC
-	.transceive_async = spi_litespi_transceive_async,
+	.transceive_async = spi_litex_transceive_async,
 #endif /* CONFIG_SPI_ASYNC */
-	.release = spi_litespi_release,
+	.release = spi_litex_release,
 };
 
-#define SPI_INIT(n)	\
-	static struct spi_litespi_data spi_litespi_data_##n = { \
-		SPI_CONTEXT_INIT_LOCK(spi_litespi_data_##n, ctx), \
-		SPI_CONTEXT_INIT_SYNC(spi_litespi_data_##n, ctx), \
-	}; \
-	static struct spi_litespi_cfg spi_litespi_cfg_##n = { \
-		.base = DT_INST_REG_ADDR_BY_NAME(n, control), \
-	}; \
-	DEVICE_DT_INST_DEFINE(n, \
-			NULL, \
-			NULL, \
-			&spi_litespi_data_##n, \
-			&spi_litespi_cfg_##n, \
-			POST_KERNEL, \
-			CONFIG_SPI_INIT_PRIORITY, \
-			&spi_litespi_api);
+#define SPI_INIT(n)                                                                                \
+	static struct spi_litex_data spi_litex_data_##n = {                                        \
+		SPI_CONTEXT_INIT_LOCK(spi_litex_data_##n, ctx),                                    \
+		SPI_CONTEXT_INIT_SYNC(spi_litex_data_##n, ctx),                                    \
+	};                                                                                         \
+	static struct spi_litex_cfg spi_litex_cfg_##n = {                                          \
+		.control_addr = DT_INST_REG_ADDR_BY_NAME(n, control),                              \
+		.status_addr = DT_INST_REG_ADDR_BY_NAME(n, status),                                \
+		.mosi_addr = DT_INST_REG_ADDR_BY_NAME(n, mosi),                                    \
+		.miso_addr = DT_INST_REG_ADDR_BY_NAME(n, miso),                                    \
+		.cs_addr = DT_INST_REG_ADDR_BY_NAME(n, cs),                                        \
+		.loopback_addr = DT_INST_REG_ADDR_BY_NAME(n, loopback),                            \
+		.clk_divider_exists = DT_INST_REG_HAS_NAME(n, clk_divider),                        \
+		.clk_divider_addr = DT_INST_REG_ADDR_BY_NAME_OR(n, clk_divider, 0)                 \
+		.data_width = DT_INST_PROP(n, data_width),                                         \
+		.max_cs = DT_INST_PROP(n, max_cs),                                                 \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(n, NULL, NULL, &spi_litex_data_##n, &spi_litex_cfg_##n, POST_KERNEL, \
+			      CONFIG_SPI_INIT_PRIORITY, &spi_litex_api);
 
 DT_INST_FOREACH_STATUS_OKAY(SPI_INIT)

suresh569/https-github.com-zephyrproject-rtos-zephyr

drivers/spi/spi_litex.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019 Antmicro <www.antmicro.com>
+ * Copyright (c) 2024 Vogl Electronic GmbH
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,21 +9,55 @@
 
 #define LOG_LEVEL CONFIG_SPI_LOG_LEVEL
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(spi_litespi);
-#include "spi_litespi.h"
-#include <stdbool.h>
+LOG_MODULE_REGISTER(spi_litex);
+
+#include "spi_litex_common.h"
+
+#define REG_CONTROL_LENGTH_OFFSET 1
+#define REG_CS_MODE_OFFSET 1
+
+struct spi_litex_data {
+	struct spi_context ctx;
+	uint8_t dfs;	/* dfs in bytes: 1,2 or 4 */
+};
+
+struct spi_litex_cfg {
+	uint32_t control_addr;
+	uint32_t status_addr;
+	uint32_t mosi_addr;
+	uint32_t miso_addr;
+	uint32_t cs_addr;
+	uint32_t loopback_addr;
+	uint32_t clk_divider_addr;
+	bool clk_divider_exists;
+	int data_width;
+	int max_cs;
+};
+
+static void spi_set_frequency(const struct device *dev, const struct spi_config *config)
+{
+	const struct spi_litex_cfg *dev_config = dev->config;
+
+	if (!dev_config->clk_divider_exists) {
+		/* The clk_divider is optional, thats why we check. */
+		LOG_WRN("No clk_divider found, can't change frequency");
+		return;
+	}
+
+	uint16_t divisor = DIV_ROUND_UP(LITEX_CPU_FREQ, config->frequency);
+
+	litex_write16(divisor, dev_config->clk_divider_addr);
+}
 
 /* Helper Functions */
-static int spi_config(const struct spi_config *config, uint16_t *control)
+static int spi_config(const struct device *dev, const struct spi_config *config)
 {
-	uint8_t cs = 0x00;
+	const struct spi_litex_cfg *dev_config = dev->config;
+	struct spi_litex_data *dev_data = dev->data;
 
-	if (config->slave != 0) {
-		if (config->slave >= SPI_MAX_CS_SIZE) {
-			LOG_ERR("More slaves than supported");
-			return -ENOTSUP;
-		}
-		cs = (uint8_t)(config->slave);
+	if (config->slave >= dev_config->max_cs) {
+		LOG_ERR("More slaves than supported");
+		return -ENOTSUP;
 	}
 
 	if (config->operation & SPI_HALF_DUPLEX) {
@@ -30,8 +65,8 @@ static int spi_config(const struct spi_config *config, uint16_t *control)
 		return -ENOTSUP;
 	}
 
-	if (SPI_WORD_SIZE_GET(config->operation) != 8) {
-		LOG_ERR("Word size must be %d", SPI_WORD_SIZE);
+	if (SPI_WORD_SIZE_GET(config->operation) > dev_config->data_width) {
+		LOG_ERR("Word size must be <= %d", dev_config->data_width);
 		return -ENOTSUP;
 	}
 
@@ -67,122 +102,147 @@ static int spi_config(const struct spi_config *config, uint16_t *control)
 	}
 
 	/* Set Loopback */
-	if (config->operation & SPI_MODE_LOOP) {
-		litex_write8(SPI_ENABLE, SPI_LOOPBACK_ADDR);
+	if (!litex_read8(dev_config->loopback_addr) != !(config->operation & SPI_MODE_LOOP)) {
+		litex_write8((config->operation & SPI_MODE_LOOP) ? 0x1 : 0x0,
+			     dev_config->loopback_addr);
 	}
+
+	/* Set CS mode, this way it is handeled by this driver. */
+	litex_write8(0x1, dev_config->cs_addr + REG_CS_MODE_OFFSET);
+
 	/* Set word size */
-	*control = (uint16_t) (SPI_WORD_SIZE_GET(config->operation)
-			<< POSITION_WORD_SIZE);
-	/* Write configurations */
-	litex_write8(cs, SPI_CS_ADDR);
-	litex_write16(*control, SPI_CONTROL_ADDR);
+	litex_write8(SPI_WORD_SIZE_GET(config->operation),
+		     dev_config->control_addr + REG_CONTROL_LENGTH_OFFSET);
+
+	dev_data->dfs = get_dfs_value(&dev_data->ctx);
+
+	spi_set_frequency(dev, config);
 
 	return 0;
 }
 
-static void spi_litespi_send(const struct device *dev, uint8_t frame,
-		             uint16_t control)
+static void spi_litex_send(const struct device *dev, uint8_t len, uint32_t frame)
 {
+	const struct spi_litex_cfg *dev_config = dev->config;
 	/* Write frame to register */
-	litex_write8(frame, SPI_MOSI_DATA_ADDR);
+	litex_write(dev_config->mosi_addr, len, frame);
 	/* Start the transfer */
-	litex_write16(control | SPI_ENABLE, SPI_CONTROL_ADDR);
+	litex_write8(0x1, dev_config->control_addr);
 	/* Wait until the transfer ends */
-	while (!(litex_read8(SPI_STATUS_ADDR)))
-		;
-}
-
-static uint8_t spi_litespi_recv(void)
-{
-    /* Return data inside MISO register */
-	return litex_read8(SPI_MISO_DATA_ADDR);
-}
-
-static void spi_litespi_xfer(const struct device *dev,
-			     const struct spi_config *config,
-			     uint16_t control)
-{
-	struct spi_context *ctx = &SPI_DATA(dev)->ctx;
-	uint32_t send_len = spi_context_longest_current_buf(ctx);
-	uint8_t read_data;
-
-	for (uint32_t i = 0; i < send_len; i++) {
-		/* Send a frame */
-		if (i < ctx->tx_len) {
-			spi_litespi_send(dev, (uint8_t) (ctx->tx_buf)[i],
-					control);
-		} else {
-			/* Send dummy bytes */
-			spi_litespi_send(dev, 0, control);
-		}
-		/* Receive a frame */
-		read_data = spi_litespi_recv();
-		if (i < ctx->rx_len) {
-			ctx->rx_buf[i] = read_data;
-		}
+	while (!(litex_read8(dev_config->status_addr))) {
+		;/* Wait */
 	}
-	spi_context_complete(ctx, dev, 0);
+}
+
+static uint32_t spi_litex_recv(const struct device *dev, uint8_t len)
+{
+	const struct spi_litex_cfg *dev_config = dev->config;
+
+	/* Return data inside MISO register */
+	return litex_read(dev_config->miso_addr, len);
+}
+
+static void spi_litex_xfer(const struct device *dev, const struct spi_config *config)
+{
+	const struct spi_litex_cfg *dev_config = dev->config;
+	struct spi_litex_data *dev_data = dev->data;
+	struct spi_context *ctx = &dev_data->ctx;
+	uint32_t txd, rxd;
+
+	/* Set CS */
+	litex_write8(BIT(config->slave), dev_config->cs_addr);
+
+	do {
+		/* Send a frame */
+		if (spi_context_tx_buf_on(ctx)) {
+			txd = *ctx->tx_buf;
+		} else {
+			txd = 0U;
+		}
+
+		spi_litex_send(dev, txd, dev_data->dfs);
+
+		spi_context_update_tx(ctx, dev_data->dfs, 1);
+
+		rxd = spi_litex_recv(dev, dev_data->dfs);
+
+		if (spi_context_rx_buf_on(ctx)) {
+			*ctx->rx_buf = rxd;
+		}
+
+		spi_context_update_rx(ctx, dev_data->dfs, 1);
+
+	} while (spi_context_tx_on(ctx) || spi_context_rx_on(ctx));
+
+	/* Clear CS */
+	litex_write8(0x0, dev_config->cs_addr);
 }
 
 /* API Functions */
 
-static int spi_litespi_transceive(const struct device *dev,
-				  const struct spi_config *config,
-				  const struct spi_buf_set *tx_bufs,
-				  const struct spi_buf_set *rx_bufs)
+static int spi_litex_transceive(const struct device *dev, const struct spi_config *config,
+				const struct spi_buf_set *tx_bufs,
+				const struct spi_buf_set *rx_bufs)
 {
-	uint16_t control = 0;
+	struct spi_litex_data *dev_data = dev->data;
+	int ret = 0;
 
-	spi_config(config, &control);
-	spi_context_buffers_setup(&SPI_DATA(dev)->ctx, tx_bufs, rx_bufs, 1);
-	spi_litespi_xfer(dev, config, control);
+	ret = spi_config(dev, config);
+	if (ret < 0) {
+		return ret;
+	}
+	spi_context_buffers_setup(&dev_data->ctx, tx_bufs, rx_bufs, dev_data->dfs);
+	spi_litex_xfer(dev, config);
 	return 0;
 }
 
 #ifdef CONFIG_SPI_ASYNC
-static int spi_litespi_transceive_async(const struct device *dev,
-					const struct spi_config *config,
-					const struct spi_buf_set *tx_bufs,
-					const struct spi_buf_set *rx_bufs,
-					struct k_poll_signal *async)
+static int spi_litex_transceive_async(const struct device *dev, const struct spi_config *config,
+				      const struct spi_buf_set *tx_bufs,
+				      const struct spi_buf_set *rx_bufs,
+				      struct k_poll_signal *async)
 {
 	return -ENOTSUP;
 }
 #endif /* CONFIG_SPI_ASYNC */
 
-static int spi_litespi_release(const struct device *dev,
-			       const struct spi_config *config)
+static int spi_litex_release(const struct device *dev, const struct spi_config *config)
 {
-	if (!(litex_read8(SPI_STATUS_ADDR))) {
+	const struct spi_litex_cfg *dev_config = dev->config;
+
+	if (!(litex_read8(dev_config->status_addr))) {
 		return -EBUSY;
 	}
 	return 0;
 }
 
 /* Device Instantiation */
-static const struct spi_driver_api spi_litespi_api = {
-	.transceive = spi_litespi_transceive,
+static const struct spi_driver_api spi_litex_api = {
+	.transceive = spi_litex_transceive,
 #ifdef CONFIG_SPI_ASYNC
-	.transceive_async = spi_litespi_transceive_async,
+	.transceive_async = spi_litex_transceive_async,
 #endif /* CONFIG_SPI_ASYNC */
-	.release = spi_litespi_release,
+	.release = spi_litex_release,
 };
 
-#define SPI_INIT(n)	\
-	static struct spi_litespi_data spi_litespi_data_##n = { \
-		SPI_CONTEXT_INIT_LOCK(spi_litespi_data_##n, ctx), \
-		SPI_CONTEXT_INIT_SYNC(spi_litespi_data_##n, ctx), \
-	}; \
-	static struct spi_litespi_cfg spi_litespi_cfg_##n = { \
-		.base = DT_INST_REG_ADDR_BY_NAME(n, control), \
-	}; \
-	DEVICE_DT_INST_DEFINE(n, \
-			NULL, \
-			NULL, \
-			&spi_litespi_data_##n, \
-			&spi_litespi_cfg_##n, \
-			POST_KERNEL, \
-			CONFIG_SPI_INIT_PRIORITY, \
-			&spi_litespi_api);
+#define SPI_INIT(n)                                                                                \
+	static struct spi_litex_data spi_litex_data_##n = {                                        \
+		SPI_CONTEXT_INIT_LOCK(spi_litex_data_##n, ctx),                                    \
+		SPI_CONTEXT_INIT_SYNC(spi_litex_data_##n, ctx),                                    \
+	};                                                                                         \
+	static struct spi_litex_cfg spi_litex_cfg_##n = {                                          \
+		.control_addr = DT_INST_REG_ADDR_BY_NAME(n, control),                              \
+		.status_addr = DT_INST_REG_ADDR_BY_NAME(n, status),                                \
+		.mosi_addr = DT_INST_REG_ADDR_BY_NAME(n, mosi),                                    \
+		.miso_addr = DT_INST_REG_ADDR_BY_NAME(n, miso),                                    \
+		.cs_addr = DT_INST_REG_ADDR_BY_NAME(n, cs),                                        \
+		.loopback_addr = DT_INST_REG_ADDR_BY_NAME(n, loopback),                            \
+		.clk_divider_exists = DT_INST_REG_HAS_NAME(n, clk_divider),                        \
+		.clk_divider_addr = DT_INST_REG_ADDR_BY_NAME_OR(n, clk_divider, 0),                \
+		.data_width = DT_INST_PROP(n, data_width),                                         \
+		.max_cs = DT_INST_PROP(n, max_cs),                                                 \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(n, NULL, NULL, &spi_litex_data_##n, &spi_litex_cfg_##n, POST_KERNEL, \
+			      CONFIG_SPI_INIT_PRIORITY, &spi_litex_api);
 
 DT_INST_FOREACH_STATUS_OKAY(SPI_INIT)